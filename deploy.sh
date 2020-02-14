2debc3976fb30d99ebec53303f7b65c9fb3a0db7
Datapotomus <matthew.therault@gmail.com>
2020-02-14 10:06:50 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Works with both Git Bash on windows and normal shell for mac and linux.
+# It does have to use your ssh key to deploy to a branch. If you don't have your ssh key set up
+# follow documentation here: https://git-scm.com/book/en/v2/Git-on-the-Server-Generating-Your-SSH-Public-Key
+
 
 # abort on errors
 set -e
